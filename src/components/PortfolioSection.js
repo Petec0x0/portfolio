a42bdae12525dc77ad0044903a29d4fab8dc9ec3
Petec0x0 <udehonyedikachi01@gmail.com>
2022-09-07 09:21:12 +0100

    Added new project (IMS web) to portfolio

--- src/components/PortfolioSection.js
+++ src/components/PortfolioSection.js
@@ -2,6 +2,7 @@ import React from 'react';
 import regtech from 'images/regtech.png';
 import cbt from 'images/cbt-app.png';
 import nruputa from 'images/nruputa.png';
+import ims_web from 'images/ims-web.png';
 import bootstrap from 'images/bootstrap.svg';
 import javascript from 'images/javascript.svg';
 import mongodb from 'images/mongodb.svg';
@@ -12,7 +13,7 @@ import php from 'images/php.svg';
 import reactjs from 'images/reactjs.svg';
 import GithubIcon from 'images/GithubIcon';
 import PopupLinkIcon from 'images/PopupLinkIcon';
-// import tailwindcss from 'images/tailwindcss.svg';
+import tailwindcss from 'images/tailwindcss.svg';
 import html from 'images/html.svg';
 import css from 'images/css.svg';
 import react_native from 'images/react-native.svg';
@@ -22,6 +23,33 @@ const PortfolioSection = () => {
         <div id="portfolio" className="bg-body py-8 px-12">
             <h1 className="text-white text-center text-3xl font-bold p-2">Portfolio</h1>
             <p className="text-white text-center md:mb-2">These are some of my personal projects, some are mobile apps, others are web apps.</p>
+            <div className="flex flex-col md:flex-row-reverse justify-center">
+                <div className="p-4 md:w-1/2 lg:w-1/3">
+                    <img src={ims_web} alt="IMS WEB" />
+                </div>
+                <div className="md:p-4 md:w-1/2">
+                    <h2 className="text-white font-bold text-xl md:px-4 md:py-6">IMS Web</h2>
+                    <p className="text-gray-300 py-2 md:px-4 md:py-0">
+                        An Inventory Managment System for tracking inventory levels, orders, sales and deliveries.
+                    </p>
+                    <div className="flex flex-row py-2 md:px-4">
+                        <span className="text-white font-bold self-center">Built With: </span>
+                        <img className="mx-1" src={nodejs} height={20} width={20} alt="Node Js" />
+                        <img className="mx-1" src={reactjs} height={20} width={20} alt="React Js" />
+                        <img className="mx-1" src={tailwindcss} height={20} width={20} alt="Tailwind CSS" />
+                        <img className="mx-1" src={mongodb} height={20} width={20} alt="MongoDB" />
+                    </div>
+                    <div className="flex flex-row md:px-4">
+                        <a title="Github Link" className="flex items-center mx-2 text-white hover:text-primary" target="_blank" rel="noopener noreferrer" href="https://github.com/Petec0x0/ims-web">
+                            <GithubIcon />
+                        </a>
+                        <a title="Demo Link" className="flex items-center mx-2 text-white hover:text-primary" target="_blank" rel="noopener noreferrer" href="/#">
+                            <PopupLinkIcon />
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <span className="p-10"></span>
             <div className="flex flex-col md:flex-row justify-center">
                 <div className="p-4 md:w-1/2 lg:w-1/3">
                     <img src={regtech} alt="Regtech" />
@@ -89,8 +117,8 @@ const PortfolioSection = () => {
                 <div className="md:p-4 md:w-1/2">
                     <h2 className="text-white font-bold text-xl md:px-4 md:py-6">Nruputa</h2>
                     <p className="text-gray-300 py-2 md:px-4 md:py-0">
-                        Nruputa is a Pomodoro time management technique app that helps 
-                        you keep your productivity high in an efficient way. 
+                        Nruputa is a Pomodoro time management technique app that helps
+                        you keep your productivity high in an efficient way.
                         Built with React Native
                     </p>
                     <div className="flex flex-row py-2 md:px-4">
